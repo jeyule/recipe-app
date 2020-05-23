99cac71e5ac8914afc0c0b4d6471d84b564a485a
ramendere <yuj@ufl.edu>
2020-05-23 11:33:18 -0400

    search and listing recipes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+/*simple recipe app created following tutorial by Hamza Mirza
+https://www.youtube.com/watch?v=PbJt7-a2274*/
+
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import apiConfig from './apiKeys';
+import Form from "./Components/Form";
+import Recipes from "./Components/Recipes"
+
+class App extends Component {
+  state = {
+    recipes: []
+  }
+  //makes api call
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://api.edamam.com/search?q=${recipeName}&app_id=${apiConfig.APP_ID}&app_key=${apiConfig.APP_KEY}&from=0&to=10`);
+    
+    const data = await api_call.json();
+    this.setState({
+      recipes: data.hits
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe}/>
+        <Recipes recipes={this.state.recipes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Recipes.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Recipes = props => (
+    <div className="container">
+        <div className="row">
+            { props.recipes.map((recipe) => {
+                console.log(recipe)
+                return (
+                    <div key={recipe.recipe.uri} className="col-md-4" style={{marginBottom:"2rem"}}>
+                        <div className="recipes__box">
+                            <img 
+                                className="recipe__box-img"
+                                src={recipe.recipe.image} 
+                                alt={recipe.recipe.label}
+                            />
+                            <div className="recipe__text">
+                                <h5 className="recipes__title">
+                                    { recipe.recipe.label.length < 20 ? `${recipe.recipe.label}` : `${recipe.recipe.label.substring(0, 25)}...`}
+                                </h5>
+                                <p className="recipes__subtitle">Publisher: 
+                                    <span> {recipe.recipe.source}</span>
+                                </p>
+                            </div>
+                            <button className="recipe_buttons">View Recipe</button>
+                        </div>
+                    </div>
+                );
+            }) }
+        </div>
+    </div>
+);
+
+export default Recipes;
